6b284c94e4d7ad973789bbf659f527e2aaaa3352
chakerox <chakerox@gmail.com>
2026-04-07 19:03:51 +0700

    deklarasi variabel struct dan isi data

--- struckdalamstruct.cpp
+++ struckdalamstruct.cpp
@@ -11,3 +11,17 @@ struct orang{
     detailAlamat alamat;
     int umur;
 };
+
+int main (){
+    //deklarasi variable struct
+    orang mhs;
+    //mengisi data
+    cout<<" isikan data nama : ";
+    cin>> mhs.nama;
+    cout<<"isikan data desa : ";
+    cin>> mhs.alamat.desa;
+    cout<<"isikan data kota : ";
+    cin>> mhs.alamat.kota;
+    cout<<"isikan data umur : ";
+    cin>> mhs.umur;
+    cout<<endl;
