980c15a4d45a372c9303fda865cec66945ae7a0b
chakerox <chakerox@gmail.com>
2026-04-07 19:04:36 +0700

    menampilkan hasil

--- struckdalamstruct.cpp
+++ struckdalamstruct.cpp
@@ -25,3 +25,10 @@ int main (){
     cout<<"isikan data umur : ";
     cin>> mhs.umur;
     cout<<endl;
+
+    cout << "Data Tampil" << endl;
+    cout << "nama : " << mhs.nama << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "kota : " << mhs.alamat.kota << endl;
+    cout << "umur : " << mhs.umur << endl;
+}
