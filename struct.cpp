46b990e15897142297f1632a12e38f2c0bb6347e
chakerox <chakerox@gmail.com>
2026-04-07 18:52:32 +0700

    membuat deklarasi variabel

--- struct.cpp
+++ struct.cpp
@@ -10,3 +10,18 @@ struct Orang
     int umur;
 
 };
+
+int main()
+
+{
+
+    //Deklarasi variabel struct
+    Orang mhs;
+    //Mengisi data struct
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << " Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << " Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
