e702cbeeaf1e1d789d6f2bcc2da0d4e1f43981ed
chakerox <chakerox@gmail.com>
2026-04-07 19:02:54 +0700

    menambahkan struktur ke dalam struktur

--- struckdalamstruct.cpp
+++ struckdalamstruct.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+struct detailAlamat{
+    string desa;
+    string kota;
+};
+
+struct orang{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
